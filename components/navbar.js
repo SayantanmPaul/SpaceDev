774af30fb097be5da08c88e013fcbe4866cd9238
Sayantanmpaul <sayantan.college6291@gmail.com>
2023-12-16 21:09:28 +0530

    conditional model update

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,19 @@
 import Image from 'next/image';
 import Logo from '../Images/SpaceDev.svg';
+import { RiBallPenLine } from "react-icons/ri";
 import SignInModel from './signinmodel';
 import { useState } from 'react';
+import { useContext } from 'react';
+import { SpacedevContext } from '../context/context';
 const Navbar = () => {
   //sign in model
   const [model, showModel]= useState(false)
 
   const closeModel=()=> showModel(false)
+
+  const {CurrentUser}=useContext(SpacedevContext)
+  console.log(CurrentUser);
+  const userImage=CurrentUser? CurrentUser.photoURL: null;
   return (
     <div>
       <div className='flex flex-row justify-between items-center mx-auto'>
@@ -19,7 +26,21 @@ const Navbar = () => {
           priority
           />
         </div>
-         <div className=' flex gap-4 flex-row'>
+         <div className=' flex lg:gap-4 gap-2 flex-row'>
+          {CurrentUser? (
+            <>
+            <button className=' bg-black text-white px-4 py-3 rounded-full text-sm duration-500 cursor-pointer flex flex-row items-center gap-1 font-medium'>Write 
+              <RiBallPenLine className=' text-white' size={20}/>
+            </button>
+            <button className='cursor-pointer'>
+              <div className=' border hover:border-indigo-500 duration-300 p-[2px] rounded-full overflow-hidden'>
+              <Image src={userImage} alt='user' width={200} height={200} className=' object-fill rounded-full overflow-hidden w-10 h-auto'/>
+              </div>
+            </button>
+            
+            </>
+          ):(
+            <>
             <button onClick={()=> showModel(true)} className=' hidden lg:block md:block cursor-pointer hover:underline text-sm '>
               Sign in
             </button>
@@ -29,6 +50,8 @@ const Navbar = () => {
             <button onClick={()=> showModel(true)} className=' bg-black text-white px-4 py-3 rounded-full text-sm duration-500 cursor-pointer'>
               Get Started
             </button>
+            </>
+          )} 
          </div>
       </div>
         <div className=' bg-slate-300 h-[1px] w-full mt-3'></div>
