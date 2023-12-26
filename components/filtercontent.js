91dbfd3fcb1bcb96e86342f2d24658452950cf94
Sayantanmpaul <sayantan.college6291@gmail.com>
2023-12-27 02:26:36 +0530

    adding hardcoded catagory filter

--- components/filtercontent.js
+++ components/filtercontent.js
@@ -1,8 +1,16 @@
-const FilterContent = () => {
+const FilterContent = ({post}) => {
   return (
     <>
-    <div className="flex lg:flex-col lg:text-sm text-[14px] font-semibold text-slate-700 flex-row ">
+    <div className="flex lg:flex-col lg:text-sm text-[14px] font-semibold text-slate-700 flex-row ]">
       Discover more what matters most to you
+    </div>
+    <div className="flex flex-col gap-2 w-full">
+      <div className=' w-full h-[0.1em] bg-slate-200 '></div>
+      <div  className=" flex flex-wrap gap-3 w-full">
+        {post.map(posts=>(
+          <div className=" bg-slate-200 px-2 p-1 rounded-md text-xs hover:bg-slate-300 duration-300 cursor-pointer text-neutral-500 w-auto  whitespace-nowrap" key={posts.id}>{posts.data.catagorypost}</div>
+        ))}
+      </div>
     </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,8 @@ export default function App() {
               <MainContent post={post} key={post.id}/>
             ))}
           </div>
-          <div className="lg:sticky relative lg:top-24 -z-10">
-            <FilterContent />
-            <div className=' w-full h-[1px] bg-slate-200 hidden lg:block'></div>
+          <div className="lg:sticky relative lg:top-24 ">
+            <FilterContent post={posts}  />
           </div>
         </div>
       </div>
