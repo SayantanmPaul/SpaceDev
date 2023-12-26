ab0402b785ebb3bb287e05010f82166ff8d42241
Sayantanmpaul <sayantan.college6291@gmail.com>
2023-12-27 03:59:55 +0530

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import { DevProvider } from '../context/context';
-
+import { Analytics } from '@vercel/analytics/react';
+ 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>SpaceDev.</title>
+        <title>SpaceDevs</title>
       </Head>
         <DevProvider>
             <Component {...pageProps} />
+            <Analytics/>
         </DevProvider>
     </>
   );
