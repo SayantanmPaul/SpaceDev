b774847560e6e6a7fe64186c192b175954299e90
Sayantanmpaul <sayantan.college6291@gmail.com>
2024-08-22 21:04:23 +0530

    form validation fix

--- components/addNewBlog.js
+++ components/addNewBlog.js
@@ -17,7 +17,7 @@ const AddNewBlogModel = ({ closemodel }) => {
 
     const route = useRouter()
 
-    const { register, formState: { errors, isSubmitting }, handleSubmit, setValue } = useForm({
+    const { register, formState: { errors, isSubmitting }, handleSubmit, setValue, trigger } = useForm({
         defaultValues: {
             title: '',
             brief: '',
@@ -36,12 +36,16 @@ const AddNewBlogModel = ({ closemodel }) => {
     const [content, setContent] = useState('')
     const [isAddingCategory, setIsAddingCategory] = useState(false)
 
-    const nextStep = () => {
-        setDirection("slide-left");
-        setTimeout(() => {
-            setStep(2);
-            setDirection("");
-        }, 300);
+    const nextStep = async () => {
+        const isValid = await trigger(['title', 'brief', 'bannerImage', 'selectedCategory', 'minutesToRead']);
+
+        if (isValid) {
+            setDirection("slide-left");
+            setTimeout(() => {
+                setStep(2);
+                setDirection("");
+            }, 300);
+        }
     };
 
     const prevStep = () => {
@@ -98,7 +102,7 @@ const AddNewBlogModel = ({ closemodel }) => {
 
     return (
         <div className=' fixed top-0 left-0 right-0 bottom-0 backdrop-brightness-50 backdrop-blur flex items-center justify-center shadow-2xl'>
-            <div className={`modal-container lg:rounded-3xl overflow-hidden flex flex-col lg:h-auto h-full max-w-4xl w-full bg-white p-8 gap-6 overflow-y-scroll no-scrollbar ${isClosing ? 'animate-fade-slideout' : 'animate-fade-slideup'} `}>
+            <div className={`modal-container lg:rounded-3xl overflow-hidden flex flex-col lg:h-auto h-full max-w-5xl w-full bg-white p-8 gap-6 overflow-y-scroll no-scrollbar ${isClosing ? 'animate-fade-slideout' : 'animate-fade-slideup'} `}>
                 <div className=' flex flex-row gap-2 items-center'>
                     <h1 className='lg:text-4xl text-[26px] font-newsletter text-indigo-700'>{step === 1 ?
                         "Thinking a new Blog ?" :
@@ -115,66 +119,83 @@ const AddNewBlogModel = ({ closemodel }) => {
                 <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-8 w-full'>
                     <div className={`step-container ${direction}`}>
                         {step === 1 && (
-                            <div className='step-content flex flex-col gap-8'>
-                                <input
-                                    type="text"
-                                    placeholder='Title*'
-                                    {...register('title', {
-                                        required: "title is required"
-                                    })}
-                                    className='outline-none placeholder:text-neutral-300 border-b-2 border-slate-200 lg:text-3xl text-xl font-newsletter focus:border-indigo-400 duration-300'
-                                />
-                                {errors.title && (
-                                    <p className='text-rose-500 text-md'>{errors.title.message}</p>
-                                )}
-                                <input
-                                    type="url"
-                                    placeholder='banner image url *'
-                                    {...register('bannerImage', {
-                                        required: 'a banner image is required',
-                                    })}
-                                    className='outline-none placeholder:text-neutral-300 border-b-2 border-slate-200 text-xl font-newsletter focus:border-indigo-400 duration-300'
-                                />
-                                {errors.bannerImage && (
-                                    <p className='text-rose-500 text-md'>{errors.bannerImage.message}</p>
-                                )}
-                                <div className='flex flex-col lg:flex-row gap-4 w-full items-start justify-between'>
+                            <div className='step-content flex flex-col gap-8 h-full justify-between w-full'>
+                                <div className='flex flex-col gap-1'>
                                     <input
                                         type="text"
-                                        placeholder='add new category *'
-                                        {...register('selectedCategory', {
-                                            required: 'please provide a relevant category'
+                                        placeholder='Title*'
+                                        {...register('title', {
+                                            required: "title is required"
                                         })}
-                                        className='outline-none placeholder:text-neutral-300 border-b-2 border-slate-200 text-md font-newsletter focus:border-indigo-400 duration-300  w-full'
+                                        onChange={() => trigger('title')}
+                                        className='outline-none placeholder:text-neutral-300 border-b-2 border-slate-200 lg:text-3xl text-xl font-newsletter focus:border-indigo-400 duration-300'
                                     />
-                                    {errors.selectedCategory && (
-                                        <p className='text-rose-500 text-md'>{errors.selectedCategory.message}</p>
+                                    {errors.title && (
+                                        <p className='text-rose-500 text-sm'>{errors.title.message}</p>
                                     )}
-                                    <div className='flex lg:flex-row flex-col gap-4 w-full'>
-                                        <span className=' min-w-fit text-md font-newsletter '>Estimated time to read :</span>
+                                </div>
+                                <div className='flex flex-col gap-2'>
+                                    <input
+                                        type="url"
+                                        placeholder='banner image url *'
+                                        {...register('bannerImage', {
+                                            required: 'a banner image is required',
+                                        })}
+                                        onChange={() => trigger('bannerImage')}
+                                        className='outline-none placeholder:text-neutral-300 border-b-2 border-slate-200 text-xl font-newsletter focus:border-indigo-400 duration-300'
+                                    />
+                                    {errors.bannerImage && (
+                                        <p className='text-rose-500 text-sm'>{errors.bannerImage.message}</p>
+                                    )}
+                                </div>
+                                <div className='flex flex-col lg:flex-row gap-4 w-full items-start justify-between'>
+                                    <div className='flex flex-col gap-2 w-full'>
                                         <input
-                                            type="number"
-                                            placeholder='minutes to read *'
-                                            {...register('minutesToRead', {
-                                                required: 'the field is required'
+                                            type="text"
+                                            placeholder='add new category *'
+                                            {...register('selectedCategory', {
+                                                required: 'please provide a relevant category'
                                             })}
+                                            onChange={() => trigger('selectedCategory')}
                                             className='outline-none placeholder:text-neutral-300 border-b-2 border-slate-200 text-md font-newsletter focus:border-indigo-400 duration-300  w-full'
                                         />
+                                        {errors.selectedCategory && (
+                                            <p className='text-rose-500 text-sm'>{errors.selectedCategory.message}</p>
+                                        )}
+                                    </div>
+                                    <div className='flex flex-col gap-2 items-end w-full'>
+                                        <div className='flex flex-row gap-4 w-full'>
+                                            <span className=' min-w-fit text-md font-newsletter '>Estimated time to read :</span>
+                                            <input
+                                                type="number"
+                                                placeholder='minutes to read *'
+                                                {...register('minutesToRead', {
+                                                    required: 'the field is required'
+                                                })}
+                                                onChange={() => trigger('minutesToRead')}
+                                                className='outline-none placeholder:text-neutral-300 border-b-2 border-slate-200 text-md font-newsletter focus:border-indigo-400 duration-300  w-full'
+                                            />
+                                        </div>
+                                        {errors.minutesToRead && (
+                                            <p className='text-rose-500 text-sm'>{errors.minutesToRead.message}</p>
+                                        )}
                                     </div>
                                 </div>
-
-                                <textarea
-                                    type="text"
-                                    rows={5}
-                                    placeholder='A brief of your blog *'
-                                    {...register('brief', {
-                                        required: 'brieft about a blog is required'
-                                    })}
-                                    className='outline-none placeholder:text-neutral-300 border-2 p-4 border-slate-200 text-xl font-newsletter focus:border-indigo-400 duration-300 rounded-xl'
-                                />
-                                {errors.brief && (
-                                    <p className='text-rose-500 text-md'>{errors.brief.message}</p>
-                                )}
+                                <div className='flex flex-col gap-2'>
+                                    <textarea
+                                        type="text"
+                                        rows={5}
+                                        placeholder='A brief of your blog *'
+                                        {...register('brief', {
+                                            required: 'brieft is required'
+                                        })}
+                                        onChange={() => trigger('brief')}
+                                        className='outline-none placeholder:text-neutral-300 border-2 p-4 border-slate-200 text-xl font-newsletter focus:border-indigo-400 duration-300 rounded-xl'
+                                    />
+                                    {errors.brief && (
+                                        <p className='text-rose-500 text-sm'>{errors.brief.message}</p>
+                                    )}
+                                </div>
                                 <div className='flex flex-row justify-between w-full '>
                                     <button
                                         type='button'
@@ -192,7 +213,7 @@ const AddNewBlogModel = ({ closemodel }) => {
                         )}
                         {step === 2 && (
                             <div className='step-content w-full flex flex-col gap-6'>
-                                <div className='h-96 w-full container border rounded-lg p-5 overflow-y-scroll' id='editorjs'>
+                                <div className='h-96 w-full container border rounded-lg p-5 overflow-y-scroll overflow-hidden' id='editorjs'>
                                     <Editor
                                         data={content}
                                         onChange={handleEditorChange}
